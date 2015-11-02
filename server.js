94f4e82cae9c6d0852753845fccf7490049086dc
Adam Taitano <adam.taitano@gmail.com>
2015-11-02 06:05:09 -0800

    add initial routes

--- server.js
+++ server.js
@@ -3,4 +3,21 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+// routes to serve the static HTML files
+app.get('/', function(req, res) {
+    res.sendfile('index.html');
+});
+// Note: route names need not match the file name
+app.get('/about', function(req, res) {
+    res.sendfile('public/about.html');
+});
+
+app.get('/contact', function(req, res) {
+    res.sendfile('public/contact.html');
+});
+
+app.get('/post', function(req, res) {
+    res.sendfile('public/post.html');
+});
+
 app.listen(3000);
